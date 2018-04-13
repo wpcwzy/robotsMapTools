df7a92a195696ad8bd7aeb48a973cad596310900
wpcwzy <wpc.369623245@live.cn>
2018-04-13 23:21:33 +0800

    Add:File read and Split. FIXME:1.Strings split wrong. 2.Wrong string init and convert

---
+++ RobotsMapTool/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace RobotsMapTool
+{
+    partial class menu
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CloseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutmeMapToolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ReadFileButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.AboutToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.TabStop = true;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenToolStripMenuItem,
+            this.SaveToolStripMenuItem,
+            this.SaveasToolStripMenuItem,
+            this.CloseToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.FileToolStripMenuItem.Text = "文件";
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.OpenToolStripMenuItem.Text = "打开";
+            this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // SaveToolStripMenuItem
+            // 
+            this.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.SaveToolStripMenuItem.Text = "保存";
+            // 
+            // SaveasToolStripMenuItem
+            // 
+            this.SaveasToolStripMenuItem.Name = "SaveasToolStripMenuItem";
+            this.SaveasToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.SaveasToolStripMenuItem.Text = "另存为";
+            // 
+            // CloseToolStripMenuItem
+            // 
+            this.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem";
+            this.CloseToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.CloseToolStripMenuItem.Text = "关闭程序";
+            this.CloseToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutmeMapToolToolStripMenuItem});
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.AboutToolStripMenuItem.Text = "关于";
+            // 
+            // AboutmeMapToolToolStripMenuItem
+            // 
+            this.AboutmeMapToolToolStripMenuItem.Name = "AboutmeMapToolToolStripMenuItem";
+            this.AboutmeMapToolToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.AboutmeMapToolToolStripMenuItem.Text = "关于Map Tool";
+            this.AboutmeMapToolToolStripMenuItem.Click += new System.EventHandler(this.AboutmeMapToolToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(102, 49);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "巡线速度";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "40";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ReadFileButton
+            // 
+            this.ReadFileButton.Enabled = false;
+            this.ReadFileButton.Location = new System.Drawing.Point(121, 48);
+            this.ReadFileButton.Name = "ReadFileButton";
+            this.ReadFileButton.Size = new System.Drawing.Size(84, 33);
+            this.ReadFileButton.TabIndex = 2;
+            this.ReadFileButton.Text = "开始分析";
+            this.ReadFileButton.UseVisualStyleBackColor = true;
+            this.ReadFileButton.Click += new System.EventHandler(this.ReadFileButton_Click);
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReadFileButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "menu";
+            this.Text = "Map Tool";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CloseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutmeMapToolToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ReadFileButton;
+    }
+}
+
---
+++ RobotsMapTool/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+
+namespace RobotsMapTool
+{
+    public partial class menu : Form
+    {
+        public bool isSaved=false;
+        string path;
+        string MapData;
+        uint speed;
+        string[] map = new string[1]; 
+
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(isSaved)
+            {
+                if(MessageBox.Show("确定要关闭程序吗？", "关闭程序", MessageBoxButtons.OKCancel,MessageBoxIcon.Asterisk)==DialogResult.OK)//文件已保存
+                {
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                if(MessageBox.Show("是否不保存直接关闭程序？", "关闭程序", MessageBoxButtons.OKCancel,MessageBoxIcon.Asterisk)==DialogResult.OK)//文件已保存
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "地图配置文件|*.ini|所有文件|*.*";
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog1.FileName;
+                Debug.WriteLine(path);
+                MapData=File.ReadAllText(path);
+                Debug.WriteLine(MapData);
+
+            }
+            if (path != "")
+            { 
+                int num;
+                ReadFileButton.Enabled = true;
+               // num=Convert.ToInt16(File.ReadLines(path));
+                //string[] map = new string[num];    //FIXME:Wrong string init and convert.
+                Debug.WriteLine(num);
+
+            }
+            else
+            {
+                ReadFileButton.Enabled = false;
+            }
+        }
+
+
+        private void AboutmeMapToolToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            speed =Convert.ToUInt16(textBox1.Text);
+            Debug.WriteLine(speed);
+        }
+
+        private void ReadFileButton_Click(object sender, EventArgs e)
+        {
+            map = MapData.Split('L');//FIXIT:Split wrong.
+            Debug.WriteLine(MapData);
+            Debug.WriteLine(map);
+        }
+    }
+}
